b6f5d6825313ad5fb664fe3dc294dee9101f2af2
Ryan Tanay <ryantanay@newevolution.org>
2016-01-21 14:20:25 -0600

    Added walls to the playfield, created a collectable object, textured it and made it a prefab

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	private Rigidbody player;
+	public float moveSpeed;
+
+	// Use this for initialization
+	void Start () {
+		player = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate () {
+		float xMove = Input.GetAxis ("Horizontal");
+		float zMove = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (xMove, 0.0f, zMove);
+
+		player.AddForce (movement * moveSpeed);
+	}
+}
